c0d8620a4d46c93b235a748e498f63a28005e550
Zhafri Shafiq <zhafrishafiq@gmail.com>
2022-07-12 00:42:18 +0800

    add Select and Insert statement

---
+++ src/main/java/io/datajek/springdata/tennisplayer/PlayerDao.java
@@ -0,0 +1,37 @@
+package io.datajek.springdata.tennisplayer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+@Repository
+public class PlayerDao {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public List<Player> getAllPlayers() {
+        String sql = "SELECT * FROM PLAYER";
+        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<Player>(Player.class));
+    }
+
+    public Player getPlayerById(int id) {
+        String sql = "SELECT * FROM PLAYER WHERE ID = ?";
+        return jdbcTemplate.queryForObject(sql,
+                new BeanPropertyRowMapper<Player>(Player.class),
+                new Object[]{id});
+    }
+
+    public int insertPlayer(Player player) {
+        String sql = "INSERT INTO PLAYER (ID, Name, Nationality, Birth_date, Titles) " +
+                "VALUES(?, ?, ?, ?, ?)";
+        return jdbcTemplate.update(sql, new Object[]
+                {player.getId(), player.getName(), player.getNationality(),
+                        new Timestamp(player.getBirthDate().getTime()),
+                        player.getTitles()});
+    }
+}
--- src/main/java/io/datajek/springdata/tennisplayer/TennisPlayerApplication.java
+++ src/main/java/io/datajek/springdata/tennisplayer/TennisPlayerApplication.java
@@ -1,13 +1,35 @@
 package io.datajek.springdata.tennisplayer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class TennisPlayerApplication {
+import java.util.Date;
 
-	public static void main(String[] args) {
-		SpringApplication.run(TennisPlayerApplication.class, args);
-	}
+@SpringBootApplication
+public class TennisPlayerApplication implements CommandLineRunner {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    PlayerDao dao;
+
+    public static void main(String[] args) {
+        SpringApplication.run(TennisPlayerApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info("Inserting Player 4: {}", dao.insertPlayer(
+                new Player(4, "Thiem", "Austria",
+                        new Date(System.currentTimeMillis()),
+                        17)
+        ));
+        logger.info("All Players Data: {}", dao.getAllPlayers());
+//		logger.info("Player with Id 3: {}", dao.getPlayerById(3));
+    }
 
 }
