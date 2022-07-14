b1e75ad0df3ac2817b1bc20fddac3887ab5e8b25
Zhafri Shafiq <zhafrishafiq@gmail.com>
2022-07-14 13:53:15 +0800

    JPA crud operations

--- src/main/java/io/datajek/springdata/tennisplayer/springdatajpa/PlayerRepository.java
+++ src/main/java/io/datajek/springdata/tennisplayer/springdatajpa/PlayerRepository.java
@@ -12,4 +12,21 @@ public class PlayerRepository {
 
     @PersistenceContext
     EntityManager entityManager;
+
+    public Player insertPlayer(Player player) {
+        return entityManager.merge(player);
+    }
+
+    public Player updatePlayer(Player player) {
+        return entityManager.merge(player);
+    }
+
+    public Player getPlayerById(int id) {
+        return entityManager.find(Player.class, id);
+    }
+
+    public void deleteById(int id) {
+        Player player = entityManager.find(Player.class, id);
+        entityManager.remove(player);
+    }
 }
